59e95a4b14e618610fe3fb3b6a26e51df6236294
sinzii <thangvx@newayict.com>
2021-04-19 22:54:39 +0700

    Update how we generate pages & styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,54 @@
 const gulp = require('gulp');
 const scss = require('gulp-sass');
+const pug = require('gulp-pug');
+const del = require('del');
 const browserSync = require('browser-sync').create();
 
-gulp.task('scss', () => {
+let mode = 'dev';
+const getOutputDir = (_mode) => {
+    if (!_mode) _mode = mode;
+
+    return _mode === 'prod' ? './dist' : './dev';
+}
+
+gulp.task('enable-prod', (done) => {
+    mode = 'prod';
+    done();
+});
+
+gulp.task('clean-dev', (done) => {
+    del([getOutputDir('dev') + '/*']);
+    done();
+});
+
+gulp.task('generate-pages', () => {
+   return gulp.src('./src/templates/pages/**/*.pug')
+       .pipe(pug({
+           verbose: true,
+           pretty: mode !== 'prod'
+       }))
+       .pipe(gulp.dest(getOutputDir()))
+       .pipe(browserSync.stream());
+});
+
+gulp.task('generate-css', () => {
     return gulp.src('./src/styles/main.scss')
-        .pipe(scss())
-        .pipe(gulp.dest('./src'))
+        .pipe(scss({
+            includePaths: ['./node_modules', './src/styles'],
+
+        }))
+        .pipe(gulp.dest(getOutputDir()))
         .pipe(browserSync.stream());
 })
 
-gulp.task('serve', () => {
+gulp.task('serve', gulp.series('clean-dev', 'generate-pages', 'generate-css', (done) => {
     browserSync.init({
         server: {
-            baseDir: './src'
+            baseDir: getOutputDir()
         }
     });
 
-    gulp.watch('./src/styles/*.scss', gulp.series('scss'));
-});
+    gulp.watch('./src/styles/**/*.scss', gulp.series('generate-css'));
+    gulp.watch('./src/templates/**/*.pug', gulp.series('generate-pages'));
+    done();
+}));
