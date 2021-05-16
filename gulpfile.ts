523b4d637cea9df8c7b95513e72f809009c6beb3
sinzii <thangvx@newayict.com>
2021-05-17 00:00:20 +0700

    Update gulp tasks & npm tasks

--- gulpfile.ts
+++ gulpfile.ts
@@ -4,11 +4,13 @@ import * as del from 'del';
 import BS from 'browser-sync';
 import scss from 'gulp-sass';
 
+import {Config} from "./src/scripts/core/model";
 import FilesSource from "./src/scripts/data/FilesSource";
 import TemplateCompiler from "./src/scripts/template/TemplateCompiler";
 const browserSync = BS.create();
 
-import config from './config.json';
+import configJson from './config.json';
+const config = Object.assign(configJson, {devMode: true}) as Config;
 
 
 class SiteGenerator {
@@ -56,13 +58,28 @@ class SiteGenerator {
         done();
     }
 
+    onProd(done) {
+        config.devMode = false;
+
+        done();
+    }
+
+    onDev(done) {
+        config.devMode = true;
+        config.baseUrl = '';
+
+        done();
+    }
+
     initTasks() {
+        gulp.task('prod', this.onProd.bind(this));
+        gulp.task('dev', this.onDev.bind(this));
         gulp.task('clean', this.clean.bind(this));
         gulp.task('generatePages', this.generatePages.bind(this));
         gulp.task('generateCss', this.generateCss.bind(this));
         gulp.task('generateJs', this.generateJs.bind(this));
         gulp.task('build', gulp.series('clean', 'generatePages', 'generateCss'));
-        gulp.task('dev', gulp.series('build', this.dev.bind(this)));
+        gulp.task('serve', gulp.series('build', this.dev.bind(this)));
     }
 }
 
