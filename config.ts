6f629dc7f41413c03346e5da12045832648cc42a
sinzii <thangvx@newayict.com>
2021-05-28 01:06:26 +0700

    Update content for demo website

--- config.ts
+++ config.ts
@@ -1,11 +1,17 @@
 import {Config, PostUrlStyle} from './src/core/model';
 
 export default {
-    baseUrl: '',
+    baseUrl: 'https://meblog.sinzii.me',
     baseContext: '',
     siteName: 'meblog',
     siteDescription: 'A DIY blog engine',
     dateTimeFormat: 'DD/MM/YYYY - HH:mm',
     dateFormat: 'DD/MM/YYYY',
-    postUrlStyle: PostUrlStyle.POST_SLUG
+    postUrlStyle: PostUrlStyle.SLUG,
+    me: {
+        fullname: 'Your Name',
+        nickname: '@meblog',
+        github: 'https://github.com/sinzii/meblog'
+    },
+    latestPosts: 10
 } as Config
