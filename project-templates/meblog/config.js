b8f4e302d4b3d2fedfcadebe800329ab1fe53267
sinzii <thangvx@newayict.com>
2021-05-30 16:05:33 +0700

    Update content & layout for meblog project template

--- project-templates/meblog/config.js
+++ project-templates/meblog/config.js
@@ -2,7 +2,7 @@ module.exports = {
     baseUrl: '',
     baseContext: '',
     siteName: 'meblog',
-    siteDescription: 'A DIY blog engine',
+    siteDescription: 'A simple static site generator',
     dateTimeFormat: 'DD/MM/YYYY - HH:mm',
     dateFormat: 'DD/MM/YYYY',
     postUrlStyle: 'POST_SLUG',
@@ -11,5 +11,29 @@ module.exports = {
         nickname: '@meblog',
         github: 'https://github.com/sinzii/meblog'
     },
+    whatToDoNext: [
+        {
+            url: 'https://github.com/sinzii/meblog#project-structure',
+            text: 'Design the new site'
+        },
+        {
+            url: 'https://github.com/sinzii/meblog#how-do-i-create-new-post',
+            text: 'Compose new posts'
+        },
+        {
+            url: 'https://github.com/sinzii/meblog#deploy-your-site-on-github',
+            text: 'Deploy your site to Github Pages'
+        }
+    ],
+    websitesBuiltWithMeblog: [
+        {
+            url: 'https://meblog.sinzii.me',
+            text: 'meblog website'
+        },
+        {
+            url: 'https://sinzii.me',
+            text: 'Thang X. Vu (@sinzii)'
+        }
+    ],
     latestPosts: 5
 }
